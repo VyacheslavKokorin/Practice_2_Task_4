fa947f26412c9f4f6308c8f37705932a8430deb4
VyacheslavKokorin <kokorin17@gmail.com>
2026-07-13 13:58:11 +0300

    Добавлен выход из аккаунта

--- app/src/routes/auth.js
+++ app/src/routes/auth.js
@@ -101,4 +101,10 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.get("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+});
+
 module.exports = router;
